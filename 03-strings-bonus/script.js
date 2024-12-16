f788b5957b9a25e298e21985ef4e6e3ea0464d6a
ValentinoBellini <valentinobellini.dev@gmail.com>
2024-12-16 14:49:39 +0100

    feat: bonus script

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,26 +6,28 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 0;
+const cityIndex = city.indexOf("Y");
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = '';
+const citySubstring =  city.substring(4, 8);
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
+const cityReplaced = city.replace("New York", "New Delhi");
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = '';
+const cityNameConcat = `${city} ${cityName}`;
 console.log(cityNameConcat); // Output: "New York City"
 
-// 5. Tutto in maiscolo
-// Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = '';
+// 5. Tutto in maiuscolo
+// Trasforma tutto in maiuscuolo la stringa della variabile hello
+const upperCaseHello = hello.toUpperCase();
 console.log(upperCaseHello); // Output: "HELLO"
+
+
